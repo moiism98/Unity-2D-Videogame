6336b6055db6c11dc4800fe781358a2dd9c6be53
moi santana <moivs@outlook.es>
2023-12-03 15:26:04 +0000

    Jump and fall animations, cap jump action

--- Platform Videogame Project/Assets/Scripts/Player/PlayerController.cs
+++ Platform Videogame Project/Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -11,18 +12,32 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
 
     [Header("Movement")]
+
     [SerializeField]
     private float speed = 3f;
     private float movement;    
 
     [Header("Jump")]
+
     [SerializeField]
     private float jumpForce = 5f;
+
+    [SerializeField]
+    private LayerMask groundLayer;
+    
+    [SerializeField]
+    private Transform groundCheck;
+
+    [SerializeField]
+    private Vector2 groundCheckSize;
+
     private void Update()
     {
         animator.SetFloat("Horizontal", movement);
 
         animator.SetFloat("Speed", Mathf.Abs(movement));
+
+        animator.SetFloat("Fall", rb.velocity.y);
     }
 
     private void FixedUpdate()
@@ -48,25 +63,42 @@ public class PlayerController : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext jump)
     {
-        if(jump.performed)
+        if(IsGrounded()) // when we add this piece of code the little jump stop working, find out what's happening and fix it up!
         {
-            // normal jump
-            
-            rb.velocity = new Vector2(transform.position.x,  jumpForce);
-        }
-        else
-        {   
-            if(jump.canceled)
+            if(jump.performed)
             {
-                // little jump
+                // normal jump
 
-                rb.velocity = new Vector2(transform.position.x, rb.velocity.y * .5f);
+                rb.velocity = new Vector2(transform.position.x, jumpForce);
             }
+            else
+            {   
+                if(jump.canceled)
+                {
+                    // little jump
+
+                    rb.velocity = new Vector2(transform.position.x, rb.velocity.y * .5f);
+                }
+            }
+
+            animator.SetTrigger("Jump");
         }
     }
 
     public void SetSpeed(float newSpeed)
     {
         speed = newSpeed;
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0, groundLayer); 
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+
+        Gizmos.DrawWireCube(groundCheck.position, groundCheckSize);
     }
 }
