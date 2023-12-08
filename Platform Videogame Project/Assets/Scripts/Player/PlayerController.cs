7f803869a6a45d518bcc54ac2e0e2b2f437a994d
moi santana <moivs@outlook.es>
2023-12-08 20:33:22 +0000

    WD: Jump under platforms!

--- Platform Videogame Project/Assets/Scripts/Player/PlayerController.cs
+++ Platform Videogame Project/Assets/Scripts/Player/PlayerController.cs
@@ -1,7 +1,9 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
@@ -52,7 +54,12 @@ public class PlayerController : MonoBehaviour
     private LayerMask ceilingLayer;
 
     [SerializeField]
+    private LayerMask platformLayer;
+
+    [SerializeField]
     private float ceilingCheckRadius = .10f;
+
+    private GameObject platform;
 
     private void Update()
     {
@@ -61,6 +68,8 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("Speed", Mathf.Abs(movement));
 
         animator.SetFloat("Fall", rb.velocity.y);
+
+        animator.SetBool("Crouch", isCrouching);
         
         ResetCrouch(); // this method reset the crouching when the crouch button is not pressed and we are not under ceilings.
     }
@@ -93,21 +102,27 @@ public class PlayerController : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext jump)
     {
-        if(IsGrounded() && !isCrouching) // when we add this piece of code the little jump stop working, find out what's happening and fix it up!
+        if(IsGrounded()) // when we add this piece of code the little jump stop working, find out what's happening and fix it up!
         {
-            if(jump.performed) // normal jump
-                rb.velocity = new Vector2(transform.position.x, jumpForce);
-            else
-            {   
-                if(jump.canceled) // little jump
-                    rb.velocity = new Vector2(transform.position.x, rb.velocity.y * .5f); 
-            }
+            if(!isCrouching)
+            {
+                if(jump.performed) // normal jump
+                    rb.velocity = new Vector2(transform.position.x, jumpForce);
+                /*else
+                {   
+                    if(jump.canceled) // little jump
+                        rb.velocity = new Vector2(transform.position.x, rb.velocity.y * .5f); 
+                }*/
 
-            animator.SetTrigger("Jump");
+                animator.SetTrigger("Jump");
+            }
+            else if(AbovePlatform())
+                StartCoroutine(JumpUnderPlatform());
         }
     }
     public void Crouch(InputAction.CallbackContext crouch)
     {
+
         if(crouch.performed) // if we're pressing the crouch button.
         {
             isCrouching = true; // we are now crouching.
@@ -130,10 +145,6 @@ public class PlayerController : MonoBehaviour
                 buttonPressed = false; // but change this value because we've released the button. NOT PRESSING IT
             }
         }
-
-        // play crouch animation.
-
-        animator.SetBool("Crouch", isCrouching);
     }
 
     private bool IsGrounded() // detects if we are touching the ground
@@ -141,9 +152,23 @@ public class PlayerController : MonoBehaviour
         return Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0, groundLayer); 
     }
 
+    private bool AbovePlatform() // detects if we are touching a platform
+    {
+        Collider2D onPlatform = Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0, platformLayer);
+
+        if(onPlatform != null)
+        {
+            platform = onPlatform.gameObject;
+
+            return true;
+        }
+
+        return false;
+    }
+
     private bool CeilingAbove() // detects if we are under ceilings
     {
-        return Physics2D.OverlapCircle(transform.position, ceilingCheckRadius ,ceilingLayer);
+        return Physics2D.OverlapCircle(transform.position, ceilingCheckRadius, ceilingLayer);
     }
 
     private void ResetCrouch()
@@ -153,9 +178,18 @@ public class PlayerController : MonoBehaviour
             crouchCol.enabled = true; // activates the top collider.
 
             isCrouching = false; // we are no longer crouching.
-
-            animator.SetBool("Crouch", isCrouching); // stop the crouch animation.
         }
+    }
+
+    private IEnumerator JumpUnderPlatform() 
+    {
+        platform.GetComponent<TilemapCollider2D>().enabled = false; // disable platorm collider.
+
+        isCrouching = false; // also the crouch animator, we are falling now.
+
+        yield return new WaitForSeconds(.35f); // wait an amount of secs
+
+        platform.GetComponent<TilemapCollider2D>().enabled = true; // and enable the platform collider again.
     }
 
     private void OnDrawGizmosSelected()
