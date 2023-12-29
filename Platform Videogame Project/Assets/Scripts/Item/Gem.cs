c67de666c58da59bdfa074bdd6548590d313ce73
moiism98 <moisessantana.tee.2014@gmail.com>
2023-12-29 11:06:39 +0000

    FIX: Score now is showed when pick up gems

--- Platform Videogame Project/Assets/Scripts/Item/Gem.cs
+++ Platform Videogame Project/Assets/Scripts/Item/Gem.cs
@@ -1,14 +1,20 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class Gem : MonoBehaviour, ItemInterface
 {
     public static event Action<int> OnGemCollect;
     [SerializeField] private int gemValue = 1000;
+    [SerializeField] private GameObject gemScore;
     public void Collect()
     {
         OnGemCollect.Invoke(gemValue);
 
         Destroy(gameObject);
+
+        gemScore.GetComponent<TextMeshPro>().text = "+ " + gemValue.ToString();
+
+        Instantiate(gemScore, transform.position, Quaternion.identity);
     }
 }
